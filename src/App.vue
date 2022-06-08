28fdd1a4ffcc3b0fb437bb0840e4d5e9d22b310d
itedya <kontakt@itedya.com>
2022-06-08 20:16:56 +0200

    Added contact section

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
   <Home />
   <Offer />
   <Technologies />
+  <Contact />
 </template>
 
 <script>
 import Home from "./components/Home";
 import Offer from "./components/Offer";
 import Technologies from "./components/Technologies";
+import Contact from "./components/Contact";
 export default {
-  components: {Offer, Home, Technologies}
+  components: {Offer, Home, Technologies, Contact}
 }
 </script>
---
+++ src/components/Contact.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="contact">
+    <div class="container mx-auto flex flex-col justify-center items-center">
+      <h1 class="text-yellow-500 font-semibold text-3xl text-center">Kontakt</h1>
+
+      <p class="text-slate-300 w-full text-center">
+        Twitch: <a class="text-purple-400" target="_blank" href="https://twitch.tv/itedya">itedya</a><br/>
+        Discord: <span class="text-blue-400">itedya#5268</span><br/>
+        Email: <span class="text-slate-400">kontakt@itedya.com</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.contact {
+  width: 100%;
+  height: 100%;
+
+  @apply bg-slate-900 py-3;
+}
+</style>

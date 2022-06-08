ee6a8b6b9ebeeea4817a0870047ad37162134fb4
itedya <kontakt@itedya.com>
2022-06-08 20:01:27 +0200

    Added technologies section

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <Home />
   <Offer />
+  <Technologies />
 </template>
 
 <script>
 import Home from "./components/Home";
 import Offer from "./components/Offer";
+import Technologies from "./components/Technologies";
 export default {
-  components: {Offer, Home}
+  components: {Offer, Home, Technologies}
 }
 </script>

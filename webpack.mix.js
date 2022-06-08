561e58c4e78fb98c5e70643ed2a604a07b8cd258
itedya <kontakt@itedya.com>
2022-06-08 20:41:25 +0200

    Added images to webpack mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,7 @@
 const mix = require("laravel-mix");
 
 mix.copy("src/index.html", "dist/index.html")
+    .copy("src/images", "dist/images")
     .sass("src/sass/main.scss", "dist/style.css")
     .options({
         postCss: [require("tailwindcss")]
